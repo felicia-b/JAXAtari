4d3f58cecb32ac6da14827f63e45df3d4cab87f6
felica-b <98695997+felica-b@users.noreply.github.com>
2025-09-08 16:23:09 +0200

    fix

--- src/jaxatari/games/jax_amidar.py
+++ src/jaxatari/games/jax_amidar.py
@@ -747,6 +747,10 @@ class JaxAmidar(JaxEnvironment[AmidarState, AmidarObservation, AmidarInfo, Amida
         return state.lives <= 0
     
     @partial(jax.jit, static_argnums=(0,))
+    def _get_reward(self, previous_state: AmidarState, state: AmidarState):
+        return self._get_env_reward(previous_state, state)
+
+    @partial(jax.jit, static_argnums=(0,))
     def _get_env_reward(self, previous_state: AmidarState, state: AmidarState):
         return state.score - previous_state.score
     
